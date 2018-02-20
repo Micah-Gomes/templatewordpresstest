17f3ac25ff18064804a8f44ed972e1bb2e7b6fe6
camilla.martins <camilla.martins@CAMILA>
2018-02-20 16:05:24 -0300

    remove categories from sidebar

--- sidebar.php
+++ sidebar.php
@@ -18,31 +18,6 @@ texto texto texto texto texto texto texto texto texto texto texto texto texto te
 
 <div class="space"></div>
 
-<h1>Flickr</h1>
-<div id="flickr"><script type="text/javascript" src="http://www.flickr.com/badge_code_v2.gne?count=6&amp;display=latest&amp;size=s&amp;layout=h&amp;source=user&amp;user=xxxxxx"></script></div>
-
-
-<h1>Categorias</h1>
-
-<?php
-$cats = explode("<br />",wp_list_categories('title_li=&echo=0&depth=0&style=none'));
-$cat_n = count($cats) - 1;
-for ($i=0;$i<$cat_n;$i++):
-if ($i<$cat_n/2):
-$cat_left = $cat_left.'<li>'.$cats[$i].'</li>';
-elseif ($i>=$cat_n/2):
-$cat_right = $cat_right.'<li>'.$cats[$i].'</li>';
-endif;
-endfor;
-?>
-<ul class="colleft">
-<?php echo $cat_left;?>
-</ul>
-<ul class="colright">
-<?php echo $cat_right;?>
-</ul>
-
-
 <h1>Últimos Posts</h1>
 <?php $lastposts = get_posts('numberposts=3&offset=0');
 foreach($lastposts as $post) : setup_postdata($post);?><ul>
